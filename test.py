f29e52a12d176e27c39fae5e87ce50317432279a
Ryan Williams <ryan@runsascoded.com>
2024-12-25 12:34:53 -0500

    append to `test.parquet`, change "num" to int32

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 df = pd.DataFrame({
-    'num': [111, 222, 333, 444, 555],
-    'str': ['aaa', 'bbb', 'ccc', 'ddd', 'eee'],
-})
+    'num': [111, 222, 333, 444, 555, 666, 777, 888],
+    'str': ['aaa', 'bbb', 'ccc', 'ddd', 'eee', 'fff', 'ggg', 'hhh'],
+}).astype({ 'num': 'int32' })
 df.to_parquet('test.parquet', index=False)
