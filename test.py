ae8638a47e0ed11f4e0f6d451d69d951b34c12c7
Ryan Williams <ryan@runsascoded.com>
2024-12-25 15:21:11 -0500

    modify DVC-dir files

--- test.py
+++ test.py
@@ -5,11 +5,11 @@ import pandas as pd
 makedirs('data', exist_ok=True)
 
 df = pd.DataFrame({
-    'num': [111, 222, 333, 444, 555],
+    'num': [11, 22, 33, 44, 55],
     'str': ['aaa', 'bbb', 'ccc', 'ddd', 'eee'],
 }).astype({ 'num': 'int32' })
 df.to_parquet('data/test.parquet', index=False)
 
 with open('data/test.txt', 'w') as f:
-    for i in range(10):
+    for i in range(15):
         print(f"{i}", file=f)
